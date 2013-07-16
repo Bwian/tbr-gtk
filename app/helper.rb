abacb0de3428a07e77919a3c39b7f507e3662864
Brian Collins <brian.pyrrho@bigpond.com>
2013-07-16 11:54:20 +1000

    Methods to check and fix directory structure

--- app/helper.rb
+++ app/helper.rb
@@ -1,3 +1,30 @@
 class Helper
-
+  
+  DIRECTORY_STRUCTURE = [
+    ['config'],
+    ['data','archive'],
+    ['logs']
+  ]
+  
+  def check_directory_structure
+    DIRECTORY_STRUCTURE.each do |dir|
+      dname = "./#{dir.join('/')}"
+      return false if !Dir.exists?(dname)
+    end  
+    true
+  end
+  
+  def fix_directory_structure
+    root = Dir.getwd
+    DIRECTORY_STRUCTURE.each do |dir|
+      Dir.chdir(root)
+      dir.each do |sub|
+        subname = "./#{sub}"
+        Dir.mkdir(subname) if !Dir.exists?(subname)
+        Dir.chdir(subname)
+      end
+    end
+    Dir.chdir(root)
+  end
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,29 @@ require'fileutils'
 require_relative '../app/helper'
 
 class TestHelper < MiniTest::Test 
-  def test_helper   
-    flunk "TODO: Check and correct directory structure"
+  
+  def setup
+    @helper = Helper.new
   end
+  
+  def test_correct_directory_structure
+    assert(@helper.check_directory_structure)   
+  end
+  
+  def test_wrong_directory_structure
+    Dir.chdir('./test')
+    refute(@helper.check_directory_structure)
+    Dir.chdir('../')
+  end
+  
+  def test_fix_directory_structure
+    FileUtils.rm_rf('./tmp')
+    Dir.mkdir('./tmp')
+    Dir.chdir('./tmp')
+    @helper.fix_directory_structure
+    assert(@helper.check_directory_structure)
+    Dir.chdir('../')
+    FileUtils.rm_rf('./tmp')
+  end
+  
 end
