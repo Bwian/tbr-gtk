b045fcb038dd87ed953ffa40477f65ffea2deb66
Brian Collins <brian.pyrrho@bigpond.com>
2013-09-24 11:29:23 +1000

    Name change to Telstra Billing Reporter

--- app/helper.rb
+++ app/helper.rb
@@ -294,10 +294,10 @@ class Helper
   
   def do_about(window)
     about = Gtk::AboutDialog.new
-    about.set_program_name "Telstra Bill Reporting"
+    about.set_program_name "Telstra Billing Reporter"
     about.set_version "1.1"
     about.set_copyright "(c) Pyrrho Pty Ltd - 2013"
-    about.set_comments "Telstra Bill Reporting parses Telstra billing files and produces individual and summary service reports.\n\nPlease note this version supports Telstra On Line Billing Service technical specification version 6.4\n"
+    about.set_comments "Telstra Billing Reporter parses Telstra billing files and produces individual and summary service reports.\n\nPlease note this version supports Telstra On Line Billing Service technical specification version 6.4\n"
     # about.set_website "www.pyrrho.com.au"
     about.set_logo(Gdk::Pixbuf.new("./images/logo_small.jpg"))
     about.run
